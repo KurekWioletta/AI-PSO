56d8572d8900ba6fa2fbce4cf1f3db85354db249
Kurek Wioletta <kurek.wioletta97@gmail.com>
2018-11-16 09:38:44 +0100

    change randomDouble()

--- mathUtils.cpp
+++ mathUtils.cpp
@@ -1,4 +1,5 @@
 #include "mathUtils.hpp"
+#include <iostream>
 
 double func(double x, double y){
     return x*x + y*y - 20*(cos(PI*x)+cos(PI*y)-2);
@@ -7,6 +8,6 @@ double func(double x, double y){
 double randomDouble(double min, double max){
     random_device rd;
     mt19937 mt(rd());
-    uniform_real_distribution<double> dist(min, std::nextafter(max, DBL_MAX));
+    uniform_real_distribution<double> dist(min, max);
     return dist(mt);
 }
